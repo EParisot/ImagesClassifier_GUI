88ea172b3dd6b5179aac2a011732cbd1f96a7858
Eric Parisot <eparisot@student.42.fr>
2018-06-16 23:32:56 +0200

    Update install_LINUX_RPI.sh

--- install_LINUX_RPI.sh
+++ install_LINUX_RPI.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-sudo apt-get install virtualenv
+sudo pip install virtualenv
 virtualenv -p python3.5 IC_GUI
 cd IC_GUI
 source bin/activate
 #dependencies
-python3 -m pip install picamera
-python3 -m pip install pillow
-python3 -m pip install pandas
-python3 -m pip install numpy
-python3 -m pip install tensorflow
-python3 -m pip install keras
-python3 -m pip install matplotlib
+apt-get install python3-tk
+pip3 install opencv-python
+pip3 install imutils
+pip3 install Pillow
+pip3 install pandas
+pip3 install numpy
+pip3 install tensorflow
+pip3 install keras
+pip3 install matplotlib
 #quit venv
 deactivate
